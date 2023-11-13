39552181e5b6d32d4b52e7c9fcadcf2db0df2620
Laima <pileliene.laima@gmail.com>
2023-11-13 16:02:21 +0200

    gitignore update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { Main } from "./components/main/Main";
 
+
 function App() {
   return (
     <>
